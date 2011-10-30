e803a4d893eefa133be288f7289376eb90545b18
Bonnie <bjcarson75@yahoo.com>
2011-10-30 18:50:48 -0500

    second commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonniedemo::Application.config.secret_token = 'aff1067f42df81ca593cb5c822dbac6d75f4e2d9c108f78c4e6dfa82eef45a3a6cd5f0b336ecb4bd5735d41eb0a3350cd7ef06e4686a5c6e26488e9bc08e854c'
